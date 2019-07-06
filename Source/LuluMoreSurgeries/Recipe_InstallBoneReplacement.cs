7e1d7673648b00387920cce9dc9dfce616f206a9
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-07-06 09:12:58 -0400

    Balancing and internal renames.

--- Source/LuluMoreSurgeries/Recipe_InstallBoneReplacement.cs
+++ Source/LuluMoreSurgeries/Recipe_InstallBoneReplacement.cs
@@ -6,15 +6,21 @@ using Verse;
 
 namespace LoonyLadle.MoreSurgeries
 {
-    public class Recipe_ReplaceBone : Recipe_InstallArtificialBodyPart
+    public class Recipe_InstallBoneReplacement : Recipe_InstallArtificialBodyPart
     {
         public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
         {
             // Check all of our hediffs for damage.
             foreach (Hediff hediff in pawn.health.hediffSet.hediffs)
             {
+                // Sanity check!
+                if (hediff.Part == null)
+                {
+                    continue;
+                }
+
                 // Is the hediff a missing part visible on a natural solid body part that can be amputated?
-                if (hediff is Hediff_MissingPart && hediff.Visible && !pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(hediff.Part) && (hediff.Part?.def.canSuggestAmputation ?? false) && (hediff.Part?.def.IsSolid(hediff.Part, hediff.pawn.health.hediffSet.hediffs) ?? false))
+                if (hediff is Hediff_MissingPart && hediff.Visible && !pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(hediff.Part) && hediff.Part.def.canSuggestAmputation && hediff.Part.def.IsSolid(hediff.Part, hediff.pawn.health.hediffSet.hediffs))
                 {
                     // It is, so yield the damaged part.
                     yield return hediff.Part;

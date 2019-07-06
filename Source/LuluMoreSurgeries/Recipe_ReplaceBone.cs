89301d6720ac059c2d29e2e3641a9da2d595c8da
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-07-06 08:41:28 -0400

    Update Recipe_ReplaceBone.cs
    Now don't allow replacing bones that cannot be amputated, instead of excepting the spine by name.

--- Source/LuluMoreSurgeries/Recipe_ReplaceBone.cs
+++ Source/LuluMoreSurgeries/Recipe_ReplaceBone.cs
@@ -1,6 +1,5 @@
 ﻿using RimWorld;
 using System.Collections.Generic;
-using System.Linq;
 using Verse;
 
 #pragma warning disable IDE1006 // Naming Styles
@@ -14,14 +13,8 @@ namespace LoonyLadle.MoreSurgeries
             // Check all of our hediffs for damage.
             foreach (Hediff hediff in pawn.health.hediffSet.hediffs)
             {
-                // Check whether our hediff's part's tag matches any exemptions.
-                if (hediff.Part?.def.tags.Contains(BodyPartTagDefOf.Spine) ?? false)
-                {
-                    // It does, so skip ahead to the next hediff.
-                    continue;
-                }
-                // Is the hediff a missing part visible on a natural solid body part?
-                if (hediff is Hediff_MissingPart && hediff.Visible && !pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(hediff.Part) && (hediff.Part?.def.IsSolid(hediff.Part, hediff.pawn.health.hediffSet.hediffs) ?? false))
+                // Is the hediff a missing part visible on a natural solid body part that can be amputated?
+                if (hediff is Hediff_MissingPart && hediff.Visible && !pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(hediff.Part) && (hediff.Part?.def.canSuggestAmputation ?? false) && (hediff.Part?.def.IsSolid(hediff.Part, hediff.pawn.health.hediffSet.hediffs) ?? false))
                 {
                     // It is, so yield the damaged part.
                     yield return hediff.Part;

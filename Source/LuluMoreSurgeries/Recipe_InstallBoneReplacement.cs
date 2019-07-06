072e8a5241e489f955c6f4155ca6848af3218cf6
Lulu <48167745+LoonyLadle@users.noreply.github.com>
2019-07-06 09:26:15 -0400

    1.3.0
    Split implants into their own mod, LuluMoreImplants.
    Split prosthetics into their own mod, LuluVanillaSurgeryExpansion.
    Bone replacements check canSuggestAmputation instead of excepting the spine by name.

--- Source/LuluMoreSurgeries/Recipe_InstallBoneReplacement.cs
+++ Source/LuluMoreSurgeries/Recipe_InstallBoneReplacement.cs
@@ -1,27 +1,26 @@
 ﻿using RimWorld;
 using System.Collections.Generic;
-using System.Linq;
 using Verse;
 
 #pragma warning disable IDE1006 // Naming Styles
 
 namespace LoonyLadle.MoreSurgeries
 {
-    public class Recipe_ReplaceBone : Recipe_InstallArtificialBodyPart
+    public class Recipe_InstallBoneReplacement : Recipe_InstallArtificialBodyPart
     {
         public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
         {
             // Check all of our hediffs for damage.
             foreach (Hediff hediff in pawn.health.hediffSet.hediffs)
             {
-                // Check whether our hediff's part's tag matches any exemptions.
-                if (hediff.Part?.def.tags.Contains(BodyPartTagDefOf.Spine) ?? false)
+                // Sanity check!
+                if (hediff.Part == null)
                 {
-                    // It does, so skip ahead to the next hediff.
                     continue;
                 }
-                // Is the hediff a missing part visible on a natural solid body part?
-                if (hediff is Hediff_MissingPart && hediff.Visible && !pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(hediff.Part) && (hediff.Part?.def.IsSolid(hediff.Part, hediff.pawn.health.hediffSet.hediffs) ?? false))
+
+                // Is the hediff a missing part visible on a natural solid body part that can be amputated?
+                if (hediff is Hediff_MissingPart && hediff.Visible && !pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(hediff.Part) && hediff.Part.def.canSuggestAmputation && hediff.Part.def.IsSolid(hediff.Part, hediff.pawn.health.hediffSet.hediffs))
                 {
                     // It is, so yield the damaged part.
                     yield return hediff.Part;

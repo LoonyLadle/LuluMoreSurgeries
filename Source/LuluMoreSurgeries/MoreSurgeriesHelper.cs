ba71be15f2ea4a65879c940be14567ca0c253a40
LoonyLadle <LoonyLadle@users.noreply.github.com>
2020-05-14 04:44:12 -0400

    tabification again

--- Source/LuluMoreSurgeries/MoreSurgeriesHelper.cs
+++ Source/LuluMoreSurgeries/MoreSurgeriesHelper.cs
@@ -6,25 +6,25 @@ using Verse;
 
 namespace LoonyLadle.MoreSurgeries
 {
-    internal static class MoreSurgeriesHelper
-    {
-        public static IEnumerable<BodyPartRecord> GetPartsPossibleToInstallArtificialPart(Pawn pawn, RecipeDef recipe)
-        {
-            foreach (BodyPartRecord record in pawn.RaceProps.body.AllParts)
-            {
-                IEnumerable<Hediff> diffs = pawn.health.hediffSet.hediffs.Where(x => x.Part == record);
-                if (diffs.Count() != 1 || diffs.First().def != recipe.addsHediff)
-                {
-                    if (record.parent == null || pawn.health.hediffSet.GetNotMissingParts().Contains(record.parent))
-                    {
-                        if (!pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(record) || pawn.health.hediffSet.HasDirectlyAddedPartFor(record))
-                        {
-                            yield return record;
-                        }
-                    }
-                }
-            }
-            yield break;
-        }
-    }
+	internal static class MoreSurgeriesHelper
+	{
+		public static IEnumerable<BodyPartRecord> GetPartsPossibleToInstallArtificialPart(Pawn pawn, RecipeDef recipe)
+		{
+			foreach (BodyPartRecord record in pawn.RaceProps.body.AllParts)
+			{
+				IEnumerable<Hediff> diffs = pawn.health.hediffSet.hediffs.Where(x => x.Part == record);
+				if (diffs.Count() != 1 || diffs.First().def != recipe.addsHediff)
+				{
+					if (record.parent == null || pawn.health.hediffSet.GetNotMissingParts().Contains(record.parent))
+					{
+						if (!pawn.health.hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(record) || pawn.health.hediffSet.HasDirectlyAddedPartFor(record))
+						{
+							yield return record;
+						}
+					}
+				}
+			}
+			yield break;
+		}
+	}
 }
--- Source/LuluMoreSurgeries/Recipe_RemoveScar.cs
+++ Source/LuluMoreSurgeries/Recipe_RemoveScar.cs
@@ -6,61 +6,61 @@ using Verse;
 
 namespace LoonyLadle.MoreSurgeries
 {
-    public class Recipe_RemoveScar : Recipe_RemoveHediff
-    {
-        // Is the hediff a permanent injury visible on a skin-covered body part?
-        public bool IsRemovableHediff(Hediff diff) => diff is Hediff_Injury && diff.IsPermanent() && diff.Visible && (diff.Part?.def.IsSkinCovered(diff.Part, diff.pawn.health.hediffSet) ?? false);
+	public class Recipe_RemoveScar : Recipe_RemoveHediff
+	{
+		// Is the hediff a permanent injury visible on a skin-covered body part?
+		public bool IsRemovableHediff(Hediff diff) => diff is Hediff_Injury && diff.IsPermanent() && diff.Visible && (diff.Part?.def.IsSkinCovered(diff.Part, diff.pawn.health.hediffSet) ?? false);
 
-        public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
-        {
-            foreach (Hediff diff in pawn.health.hediffSet.hediffs)
-            {
-                if (diff.Part != null)
-                {
-                    if (IsRemovableHediff(diff))
-                    {
-                        yield return diff.Part;
-                    }
-                }
-            }
-            yield break;
-        }
+		public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
+		{
+			foreach (Hediff diff in pawn.health.hediffSet.hediffs)
+			{
+				if (diff.Part != null)
+				{
+					if (IsRemovableHediff(diff))
+					{
+						yield return diff.Part;
+					}
+				}
+			}
+			yield break;
+		}
 
-        public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
-        {
-            // Moved this line earlier in code to allow getting its label in the billDoer block.
-            Hediff hediff = pawn.health.hediffSet.hediffs.Find(x => x.Part == part && IsRemovableHediff(x));
+		public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+		{
+			// Moved this line earlier in code to allow getting its label in the billDoer block.
+			Hediff hediff = pawn.health.hediffSet.hediffs.Find(x => x.Part == part && IsRemovableHediff(x));
 
-            if (billDoer != null)
-            {
-                if (CheckSurgeryFail(billDoer, pawn, ingredients, part, bill))
-                {
-                    return;
-                }
-                TaleRecorder.RecordTale(TaleDefOf.DidSurgery, new object[]
-                {
-                    billDoer,
-                    pawn
-                });
-                if (PawnUtility.ShouldSendNotificationAbout(pawn) || PawnUtility.ShouldSendNotificationAbout(billDoer))
-                {
-                    string text;
-                    if (!recipe.successfullyRemovedHediffMessage.NullOrEmpty())
-                    {
-                        text = string.Format(recipe.successfullyRemovedHediffMessage, billDoer.LabelShort, pawn.LabelShort);
-                    }
-                    else
-                    {
-                        text = "MessageSuccessfullyRemovedHediff".Translate(billDoer.LabelShort, pawn.LabelShort, hediff.LabelBase.Named("HEDIFF"), billDoer.Named("SURGEON"), pawn.Named("PATIENT"));
-                    }
-                    Messages.Message(text, pawn, MessageTypeDefOf.PositiveEvent, true);
-                }
-            }
+			if (billDoer != null)
+			{
+				if (CheckSurgeryFail(billDoer, pawn, ingredients, part, bill))
+				{
+					return;
+				}
+				TaleRecorder.RecordTale(TaleDefOf.DidSurgery, new object[]
+				{
+					billDoer,
+					pawn
+				});
+				if (PawnUtility.ShouldSendNotificationAbout(pawn) || PawnUtility.ShouldSendNotificationAbout(billDoer))
+				{
+					string text;
+					if (!recipe.successfullyRemovedHediffMessage.NullOrEmpty())
+					{
+						text = string.Format(recipe.successfullyRemovedHediffMessage, billDoer.LabelShort, pawn.LabelShort);
+					}
+					else
+					{
+						text = "MessageSuccessfullyRemovedHediff".Translate(billDoer.LabelShort, pawn.LabelShort, hediff.LabelBase.Named("HEDIFF"), billDoer.Named("SURGEON"), pawn.Named("PATIENT"));
+					}
+					Messages.Message(text, pawn, MessageTypeDefOf.PositiveEvent, true);
+				}
+			}
 
-            if (hediff != null)
-            {
-                pawn.health.RemoveHediff(hediff);
-            }
-        }
-    }
+			if (hediff != null)
+			{
+				pawn.health.RemoveHediff(hediff);
+			}
+		}
+	}
 }

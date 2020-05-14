30de85ef37e72b5ffa405f3b96503baa66e32f10
Lulu <48167745+LoonyLadle@users.noreply.github.com>
2020-05-14 05:00:27 -0400

    1.6.0
    Updated for RimWorld version 1.1.

--- Source/LuluMoreSurgeries/Recipe_InstallCosmeticBodyPart.cs
+++ Source/LuluMoreSurgeries/Recipe_InstallCosmeticBodyPart.cs
@@ -7,24 +7,24 @@ using Verse;
 
 namespace LoonyLadle.MoreSurgeries
 {
-    public class Recipe_InstallCosmeticBodyPart : Recipe_InstallArtificialBodyPart
-    {
-        public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
-        {
-            foreach (BodyPartRecord part in MoreSurgeriesHelper.GetPartsPossibleToInstallArtificialPart(pawn, recipe))
-            {
-                if (part.def.canSuggestAmputation && part.def.beautyRelated)
-                {
-                    foreach (Hediff hediff in pawn.health.hediffSet.hediffs.Where(x => x.Part == part))
-                    {
-                        if ((hediff is Hediff_Injury && hediff.IsPermanent()) || hediff is Hediff_MissingPart)
-                        {
-                            yield return part;
-                        }
-                    }
-                }
-            }
-            yield break;
-        }
-    }
+	public class Recipe_InstallCosmeticBodyPart : Recipe_InstallArtificialBodyPart
+	{
+		public override IEnumerable<BodyPartRecord> GetPartsToApplyOn(Pawn pawn, RecipeDef recipe)
+		{
+			foreach (BodyPartRecord part in MoreSurgeriesHelper.GetPartsPossibleToInstallArtificialPart(pawn, recipe))
+			{
+				if (part.def.canSuggestAmputation && part.def.beautyRelated)
+				{
+					foreach (Hediff hediff in pawn.health.hediffSet.hediffs.Where(x => x.Part == part))
+					{
+						if ((hediff is Hediff_Injury && hediff.IsPermanent()) || hediff is Hediff_MissingPart)
+						{
+							yield return part;
+						}
+					}
+				}
+			}
+			yield break;
+		}
+	}
 }
